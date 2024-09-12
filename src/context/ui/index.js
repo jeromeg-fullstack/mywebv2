c493d626e7d00165a2b85ca76c76a84756d2d385
jeromeg-fullstack <jerome.fullstack@gmail.com>
2024-09-12 19:39:38 +0800

    created Ui Ctx

---
+++ src/context/ui/index.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const SiteUiContext = createContext();
+
+const SiteUiProvider = ({ children }) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [isInit, setIsInit] = useState(false);
+
+	const state = useMemo(() => ({ isLoading, isOpen, isInit }), [isLoading, isOpen, isInit]);
+	const dispatch = useMemo(() => ({ setIsLoading, setIsOpen, setIsInit }), []);
+
+	return <SiteUiContext.Provider value={{ state, dispatch }}>{children}</SiteUiContext.Provider>;
+};
+
+export { SiteUiContext, SiteUiProvider };
