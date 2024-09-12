85a02883735c215351d8bbf314abfce7e5ef1c4e
jeromeg-fullstack <jerome.fullstack@gmail.com>
2024-09-12 17:55:32 +0800

    feat: add getLayout function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,17 @@ import { CssBaseline } from "@mui/material";
 import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "@/utils"; // Make sure to have this utility
 import SiteThemeProvider from "@/context/theme";
+import UserLayout from "@/layout/user-layout";
 
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  const getLayout = Component.getLayout || ((page) => <UserLayout>{page}</UserLayout>);
 	return (
 		<CacheProvider value={emotionCache}>
 			<SiteThemeProvider>
 				<CssBaseline />
-				<Component {...pageProps} />
+				{getLayout(<Component {...pageProps} />)}
 			</SiteThemeProvider>
 		</CacheProvider>
 	);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
+
 
 export default function Home() {
 	return (
