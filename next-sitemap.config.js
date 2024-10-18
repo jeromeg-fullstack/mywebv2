f654fd7b149c6fc6e283744d14d964192d604964
jeromeg-fullstack <jerome.fullstack@gmail.com>
2024-10-18 08:47:35 +0800

    refactor: sitemap code

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://smartva.studio", // Replace with your actual domain
+	generateRobotsTxt: true, // Optional: Generate a robots.txt file
+	sitemapSize: 5000 // Optional: Split sitemap into multiple files if needed
+};
