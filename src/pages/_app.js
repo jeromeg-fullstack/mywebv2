4a0591cde0a9af6cbd7b4fee2af7b4e19b63f289
jeromeg-fullstack <jerome.fullstack@gmail.com>
2024-11-01 10:39:30 +0800

    feat: add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import "@/styles/icomoon.css";
 import "@/styles/testimonials.css";
@@ -13,9 +15,37 @@ const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
 	const getLayout = Component.getLayout || ((page) => <UserLayout>{page}</UserLayout>);
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			window.gtag("config", "G-V8MGK13ZL0", { page_path: url });
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<>
 			<Analytics />
+			{/* Google Tag (gtag.js) */}
+			<Script
+				strategy="afterInteractive"
+				src="https://www.googletagmanager.com/gtag/js?id=G-V8MGK13ZL0"
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V8MGK13ZL0');
+          `
+				}}
+			/>
 			<CacheProvider value={emotionCache}>
 				<SiteThemeProvider>
 					<SiteUiProvider>
